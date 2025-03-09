604ffa4f0cf3f6a9f65dd63706169a25252bfecb
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-03-09 19:52:23 +0100

    new api key

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddScoped<FootService>();
 builder.Services.AddScoped(sp =>
 {
     var client = new HttpClient { BaseAddress = new Uri("https://v3.football.api-sports.io/") };
-    client.DefaultRequestHeaders.Add("x-apisports-key", "8890215dae5e0601bcb40f0822ce74c");
+    client.DefaultRequestHeaders.Add("x-apisports-key", "8890215dae5e0601bcb40f0822ce74cb");
     return client;
 });
 await builder.Build().RunAsync();
