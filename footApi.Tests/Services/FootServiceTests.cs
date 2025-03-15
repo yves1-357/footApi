4daed980caf1309484f24f8a0334e2bcb9ef0d82
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-03-15 11:25:13 +0100

    ajout fichier tests

---
+++ footApi.Tests/Services/FootServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using footApi.Services;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace footApi.Tests.Services;
+
+[TestClass]
+[TestSubject(typeof(FootService))]
+public class FootServiceTests
+{
+
+    [TestMethod]
+    public void ConvertToBelgiumTime_WithNSStatus_ReturnsHourInBelgium()
+    {
+        var footService = new FootService(null!);
+
+        var status = new Status { Short = "NS" };
+
+        string utcTime = "2025-02-25T12:00:00+00:00";
+
+        var result = footService.ConvertToBelgiumTime(utcTime, status);
+        Assert.AreEqual("13:00", result);
+    }
+
+}
--- footApi.Tests/obj/Debug/net9.0/footApi.Tests.AssemblyInfo.cs
+++ footApi.Tests/obj/Debug/net9.0/footApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("footApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d23dd6b7ede9df3b74bf0226fb27b957580e118")]
 [assembly: System.Reflection.AssemblyProductAttribute("footApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("footApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
